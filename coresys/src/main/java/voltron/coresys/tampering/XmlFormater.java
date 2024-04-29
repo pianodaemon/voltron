81c802fb9e2e831035d6d536af7354e9332f3528
Edwin Plauchu <j4nusx@yahoo.com>
2024-04-29 11:17:05 -0500

    Schema is provided, it is used for validation
    Signed-off-by: Edwin Plauchu <j4nusx@yahoo.com>

--- coresys/src/main/java/voltron/coresys/tampering/XmlFormater.java
+++ coresys/src/main/java/voltron/coresys/tampering/XmlFormater.java
@@ -5,15 +5,20 @@ import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.InputStream;
 import java.io.IOException;
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.transform.stream.StreamSource;
+import org.xml.sax.SAXException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-
 import lombok.Getter;
 import voltron.coresys.VoltronException;
 
@@ -23,6 +28,7 @@ public abstract class XmlFormater implements OutputFormater<ByteArrayOutputStrea
 
     private static final String XML_TAILORED_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
     private static final String CTX_PATH = "com.immortalcrab.voltron.portage";
+    private static final String WLP_LIBERTY_FULL_SCHEMA = "wlp/liberty_full.xsd";
 
     private final JAXBElement<ServerType> server;
 
@@ -35,7 +41,8 @@ public abstract class XmlFormater implements OutputFormater<ByteArrayOutputStrea
     @Override
     public ByteArrayOutputStream render() throws VoltronException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        marshalling(tailorHandler(), os);
+        Schema schema = loadSchema(getClass().getClassLoader().getResourceAsStream(WLP_LIBERTY_FULL_SCHEMA));
+        marshalling(tailorHandler(), os, schema);
         return os;
     }
 
@@ -48,13 +55,14 @@ public abstract class XmlFormater implements OutputFormater<ByteArrayOutputStrea
         }
     }
 
-    private static void marshalling(JAXBElement<ServerType> server, ByteArrayOutputStream baos) throws VoltronException {
+    private static void marshalling(JAXBElement<ServerType> server, ByteArrayOutputStream baos, Schema schema) throws VoltronException {
         Marshaller marshaller;
         try {
             JAXBContext context = JAXBContext.newInstance(CTX_PATH);
             marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+            marshaller.setSchema(schema);
             baos.write(XML_TAILORED_HEADER.getBytes());
             marshaller.marshal(server, baos);
         } catch (IOException | JAXBException ex) {
@@ -73,5 +81,14 @@ public abstract class XmlFormater implements OutputFormater<ByteArrayOutputStrea
 
     private static <T> JAXBElement<T> unmarshalling(String filePath) throws VoltronException {
         return (JAXBElement<T>) unmarshalling(new File(filePath));
+    }
+
+    public static Schema loadSchema(InputStream schemaStream) throws VoltronException {
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            return factory.newSchema(new StreamSource(schemaStream));
+        } catch (SAXException ex) {
+            throw new VoltronException("Failed to load XML schema.", ex);
+        }
     }
 }
