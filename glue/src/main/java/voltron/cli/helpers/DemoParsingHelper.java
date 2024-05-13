edcc645e1cb33429b85ecffa12f629a931c65bed
Edwin Plauchu <pianodaemon@gmail.com>
2024-05-13 10:18:51 -0600

    Create DemoParsingHelper.java

---
+++ glue/src/main/java/voltron/cli/helpers/DemoParsingHelper.java
@@ -0,0 +1,45 @@
+package voltron.cli.helpers;
+
+import java.util.Map;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import voltron.coresys.RestClientException;
+import voltron.coresys.SculptorException;
+import voltron.coresys.alterations.Alteration00;
+import voltron.coresys.tampering.XmlFormater;
+import voltron.coresys.wlp.mbeans.LibertyConnREST;
+
+public class DemoParsingHelper {
+
+    public static final Option OPTION_SUB_CMD = Option.builder("d")
+            .longOpt("description")
+            .required(false)
+            .hasArg(true)
+            .desc("This is a demo option upon cli")
+            .build();
+
+    public static void tie(String[] args, CommandLineParser parser, Options options) throws SculptorException, RestClientException {
+        CommandLine cmdLine;
+        String desc;
+        try {
+            cmdLine = parser.parse(options, args);
+            if (!cmdLine.hasOption('d')) {
+                return;
+            }
+            desc = cmdLine.getOptionValue('d');
+        } catch (ParseException ex) {
+            throw new SculptorException(ex);
+        }
+
+        System.out.println("xxx: " + desc);
+        XmlFormater xmlFormater;
+        xmlFormater = new Alteration00("server_original.xml", desc, false);
+        xmlFormater.renderFeaturingSave("server.xml");
+        LibertyConnREST lc = new LibertyConnREST("127.0.0.1", 9443);
+        Map<String, String> m = lc.inquiryAllApplicationStatus();
+        m.forEach((key, value) -> System.out.println(key + "->" + value));
+    }
+}
